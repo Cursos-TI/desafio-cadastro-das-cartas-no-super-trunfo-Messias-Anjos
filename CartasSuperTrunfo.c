4ebf3bd380e1668a9d9907d3650fcb1dc284ab58
Messias Alves <messiasalves16k@gmail.com>
2025-03-28 16:58:32 +0000

    atribuir a media

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,10 +13,11 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    int Populacao;
-    float Area;
-    float Pib;
+    int Populacao1, Populacao2;
+    float Area1, Area2;
+    float Pib1, Pib2;
     int PontosTuristicos;
+    float media1, media11, media2, media22;
 
     
     // Cadastro das Cartas:
@@ -27,15 +28,15 @@ int main() {
 
   //Aqui é o numero de população.
   printf("Digite o número de população: ");
-  scanf("%d", &Populacao);
+  scanf("%d", &Populacao1);
 
   //aqui é o número de Área.
   printf("Digite sua Área: ");
-  scanf("%f", &Area);
+  scanf("%f", &Area1);
 
   //aqui é o número de Pib.
   printf("Digite o número de Pib: ");
-  scanf("%f", &Pib);
+  scanf("%f", &Pib1);
 
   //Aqui é o número de pontos turisticos.
   printf("Digite o nume de Pontos Turistico: ");
@@ -57,15 +58,15 @@ int main() {
 
   //Aqui é o numero de população.
   printf("Digite o número de população: ");
-  scanf("%d", &Populacao);
+  scanf("%d", &Populacao2);
 
   //aqui é o número de Área.
   printf("Digite sua Área: ");
-  scanf("%f", &Area);
+  scanf("%f", &Area2);
 
   //aqui é o número de Pib.
   printf("Digite o número de Pib: ");
-  scanf("%f", &Pib);
+  scanf("%f", &Pib2);
 
   //Aqui é o número de pontos turisticos.
   printf("Digite o nume de Pontos Turistico: ");
@@ -78,9 +79,9 @@ int main() {
   //Carta numero 1
   printf("Carta 1\n");
 
-  printf("Sua população e de : %d \n", Populacao);
-  printf("Sua área é de: %.1f \n", Area );  
-  printf("Seu pib é de: %.1f \n", Pib);
+  printf("Sua população e de : %d \n", Populacao1);
+  printf("Sua área é de: %.1f \n", Area1 );  
+  printf("Seu pib é de: %.1f \n", Pib1);
   printf("Seu número de Pontos Turisticos é de: %d \n\n", PontosTuristicos);
 
   printf("\n\n");
@@ -89,10 +90,31 @@ int main() {
   //carta numero 2
   printf("Carta 2\n");
 
-  printf("Sua população e de : %d \n", Populacao);
-  printf("Sua área é de: %.1f \n", Area );  
-  printf("Seu pib é de: %.1f \n", Pib);
+  printf("Sua população e de : %d \n", Populacao2);
+  printf("Sua área é de: %.1f \n", Area2 );  
+  printf("Seu pib é de: %.1f \n", Pib2);
   printf("Seu número de Pontos Turisticos é de: %d \n\n", PontosTuristicos);
+
+  media1 = (float)(Pib1 / Area1);
+  media11 = (float)(Populacao1 / Area1);
+  
+
+  media2 = (float)(Pib2 / Area2);
+  media22 = (float)(Populacao2 / Area2);
+
+  printf("A media de Pib por Area da carta 1 é de: %.2f", media1);
+  printf("\n");
+
+  printf("A media de Populacao por Area da carta 1 é de: %.2f", media11);
+  printf("\n");
+
+
+  printf("A media de Pib por Area da carta 2 é de: %.2f", media2);
+  printf("\n");
+
+  printf("A media de Populacao por Area, da carta 2 é de: %.2f", media22);
+  printf("\n");
+
 
     
     return 0;
